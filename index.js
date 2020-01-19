1dc3f987bee1eeb9a17cad36958e2f403f4a997b
MaidenSyn <maidensyn@gmail.com>
2020-01-19 00:56:57 -0600

    updated token

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
 
-const token = 'NjY3NTc0OTA5ODQ5MDQyOTU1.XiP53w.aCgRHTRLKjvW5m4prDKEyDWPfqY'
+const token = 'NjY3NTc0OTA5ODQ5MDQyOTU1.XiP9Gg.--pDaA4nkt4oZlbYCEnbgMvnNnA'
 
 bot.on('ready', () =>{
     console.log('This bot is online');
